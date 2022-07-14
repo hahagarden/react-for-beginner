7af093aba66235d26eaf2a2099686fc544e25457
hahagarden <tingyuan413@gmail.com>
2022-07-14 22:35:34 +0900

    Make movie list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 function Todo() {
   const [toDo, setToDo] = useState("");
@@ -40,6 +41,7 @@ function Coin() {
         setLoading(false);
       });
   }, []);
+  console.log(coins);
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
@@ -58,4 +60,40 @@ function Coin() {
   );
 }
 
-export { Todo, Coin };
+function MovieList() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export { Todo, Coin, MovieList };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Todo } from "./App";
 import { Coin } from "./App";
+import { MovieList } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Coin />
-  </React.StrictMode>
-);
+root.render(<MovieList />);
