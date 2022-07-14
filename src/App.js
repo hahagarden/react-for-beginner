c769f401d19d6cb7f713b6f570cbae048680edd6
hahagarden <tingyuan413@gmail.com>
2022-07-14 20:36:11 +0900

    Make coin list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-function App() {
+function Todo() {
   const [toDo, setToDo] = useState("");
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
@@ -29,4 +29,33 @@ function App() {
   );
 }
 
-export default App;
+function Coin() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin, index) => (
+            <option key={index}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
+
+export { Todo, Coin };
